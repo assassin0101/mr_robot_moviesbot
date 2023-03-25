cbf0f20d017e10355cb6a872414b81e4103cf22e
assassin0101 <118814267+assassin0101@users.noreply.github.com>
2023-03-25 09:07:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -98,6 +98,9 @@ async def start(client, message):
             InlineKeyboardButton('🔍 𝚂𝚎𝚊𝚛𝚌𝚑', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🤖 𝚄𝚙𝚍𝚊𝚝𝚎𝚜', url='https://t.me/+R51KNo5WW3djOTU9')
             ],[
+            InlineKeyboardButton('⚜️ 𝙻𝚎𝚎𝚌𝚑 𝙶𝚛𝚘𝚞𝚙', url='https://t.me/+YwyVIDbBMQRhZDdl'),
+            InlineKeyboardButton('⚜️ 𝙼𝚘𝚟𝚒𝚎 𝙶𝚛𝚘𝚞𝚙', url='𝚑𝚝𝚝𝚙𝚜://𝚝.𝚖𝚎/+𝚓2𝙲𝚀𝚅7𝙱𝚑𝙼4𝚝𝚖𝙽𝚣𝚉𝚕')
+            ],[
             InlineKeyboardButton('ℹ️ 𝙷𝚎𝚕𝚙', callback_data='help'),
             InlineKeyboardButton('😊 𝙰𝚋𝚘𝚞𝚝', callback_data='about')
             ],[
