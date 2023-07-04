054f7a00fcbd05f4e45ab797d2bc05c6b1f20525
assassin0101 <118814267+assassin0101@users.noreply.github.com>
2023-07-04 11:03:36 +0530

    commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -248,6 +248,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('𝐉𝐨𝐢𝐧 𝐎𝐮𝐫 𝐌𝐞𝐦𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥', url='https://t.me/memespoor') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
